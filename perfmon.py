3e779f2efbce4d0fb63214353002b39ffa170299
LITTLEONE\Mattias <mattias-fredriksson91@hotmail.se>
2020-06-21 07:48:41 +0200

    Moved separated performance sampler functions

--- perfmon.py
+++ perfmon.py
@@ -18,7 +18,7 @@
 
 # <pep8 compliant>
 
-# Script copyright (C) Campbell Barton, Bastien Montagne
+# Script copyright (C) Campbell Barton, Bastien Montagne, Mattias Fredriksson
 
 
 # ##### Performance monitor #####
@@ -28,6 +28,23 @@ DO_PERFMON = True
 if DO_PERFMON:
 
     import time
+
+    def new_sampler(init=False):
+        if init:    return ([time.process_time()], [])
+        else:       return ([], [])
+
+    def begin_sample(sampler):
+        sampler[0].append(time.process_time())
+
+    def end_sample(sampler):
+        sampler[1].append(time.process_time())
+
+    def analyze_sample(sampler, first_sample=None, end_sample=None):
+        import numpy
+        diff = numpy.subtract(sampler[1], sampler[0])[first_sample:end_sample]
+        sum = numpy.sum(diff)
+        mean = numpy.sum(diff) / len(diff)
+        return sum, mean
 
     class PerfMon():
         def __init__(self):
@@ -67,6 +84,19 @@ if DO_PERFMON:
 
 
 else:
+
+    def new_sampler(init=False):
+        return None
+
+    def begin_sample(sampler):
+        pass
+
+    def end_sample(sampler):
+        pass
+
+    def analyze_sample(sampler, first_sample=None, end_sample=None):
+        return 0.0, 0.0
+
     class PerfMon():
         def __init__(self):
             pass
@@ -78,4 +108,7 @@ else:
             pass
 
         def step(self, message=""):
+            pass
+
+        def message(self, message):
             pass
--- pyfuncs.py
+++ pyfuncs.py
@@ -1,26 +1,3 @@
-import time
-
-def new_sampler(init=False):
-    if init:    return ([time.process_time()], [])
-    else:       return ([], [])
-
-def begin_sample(sampler):
-    sampler[0].append(time.process_time())
-
-def end_sample(sampler):
-    sampler[1].append(time.process_time())
-
-def mean_sample(sampler, first_sample=None, end_sample=None):
-    import numpy
-    diff = numpy.subtract(sampler[1], sampler[0])[first_sample:end_sample]
-    return numpy.sum(diff) / len(diff)
-
-def analyze_sample(sampler, first_sample=None, end_sample=None):
-    import numpy
-    diff = numpy.subtract(sampler[1], sampler[0])[first_sample:end_sample]
-    sum = numpy.sum(diff)
-    mean = numpy.sum(diff) / len(diff)
-    return sum, mean
 
 def islist(N):
 	"""
