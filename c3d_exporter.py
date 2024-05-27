15113e070fbd4698f16bae5ae0b6c7069e4f4205
Pascal Jacob <poscoljo@aol.com>
2024-05-27 16:03:26 +0200

    Write dummy data on export

--- c3d_exporter.py
+++ c3d_exporter.py
@@ -3,8 +3,6 @@ import numpy as np
 from .c3d.c3d import Writer
 
 def export_c3d(filepath):
-    # Create a new C3D writer object
-    writer = Writer()
 
     # Get the scene data from Blender
     scene = bpy.context.scene
@@ -15,48 +13,55 @@ def export_c3d(filepath):
     # Determine the number of points (bones)
     point_count = sum(len(obj.pose.bones) for obj in scene.objects if obj.type == 'ARMATURE')
 
+    frame_rate = 1
+    point_count = 10
+
+    # Create a new C3D writer object
+    writer = Writer(frame_rate,frame_rate)
+
     # Access the header once and set attributes
-    header = writer.header
-    header.point_count = point_count
-    header.first_frame = frame_start
-    header.last_frame = frame_end
-    header.scale_factor = -1
-    header.frame_rate = frame_rate
+    # header = writer.header
+    # header.point_count = point_count
+    # header.analog_count = point_count
+    # header.first_frame = frame_start
+    # header.last_frame = frame_end
+    # header.scale_factor = -1
+    # header.frame_rate = frame_rate
 
-    # Collect bone position data over the frames
-    frames = frame_end - frame_start + 1
-    points = np.zeros((frames, point_count, 3))
-    
+    writer.header.analog_per_frame = 5
+
+    points = np.zeros((10, 5), np.float32)
+    analog = np.zeros((10, 5), np.float32)
+
+    writer.set_point_labels(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"])
+    #writer.set_analog_labels = ["aA", "aB", "aC", "aD", "aE", "aF", "aG", "aH", "aI", "aJ"]
+    writer.set_analog_labels = []
+
+    writer.add_frames((points, analog))
+
+        
     # Initialize a list of bone names to keep track of the order of bones
-    bone_names = []
-    bone_index = 0
-    for obj in scene.objects:
-        if obj.type == 'ARMATURE':
-            for bone in obj.pose.bones:
-                bone_names.append((obj.name, bone.name))
-                bone_index += 1
+    # bone_names = []
+    # bone_index = 0
+    # for obj in scene.objects:
+    #     if obj.type == 'ARMATURE':
+    #         for bone in obj.pose.bones:
+    #             bone_names.append((obj.name, bone.name))
+    #             bone_index += 1
     
-    # Iterate over frames and collect positions
-    for frame in range(frame_start, frame_end + 1):
-        scene.frame_set(frame)
-        bone_index = 0
-        for obj in scene.objects:
-            if obj.type == 'ARMATURE':
-                for bone in obj.pose.bones:
-                    bone_world_matrix = obj.matrix_world @ bone.matrix
-                    bone_head = bone_world_matrix.to_translation()
-                    points[frame - frame_start, bone_index, :] = bone_head
-                    bone_index += 1
-
-    # Prepare the frames for the C3D writer
-    frame_data = []
-    for frame in range(frames):
-        point_data = points[frame]
-        analog_data = np.zeros((0,))  # Empty analog data as we don't have analog measurements
-        frame_data.append((point_data, analog_data))
-
-    # Write the frames to the C3D file
-    writer.add_frames(frame_data)
+    # # Iterate over frames and collect positions
+    # for frame in range(frame_start, frame_end + 1):
+    #     scene.frame_set(frame)
+    #     bone_index = 0
+    #     frame_data = np.zeros((point_count,3))
+    #     for obj in scene.objects:
+    #         if obj.type == 'ARMATURE':
+    #             for bone in obj.pose.bones:
+    #                 bone_world_matrix = obj.matrix_world @ bone.matrix
+    #                 bone_head = bone_world_matrix.to_translation()
+    #                 frame_data[bone_index] = bone_head.xyz
+    #                 bone_index += 1
+    #                 writer.add_frames(frame_data)
 
     # Save the C3D file
     with open(filepath, 'wb') as f:
