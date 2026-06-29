c6c4bfd31166cbfdf3aa1b32ef5c3eabb1234ab2
Osares <mattias-fredriksson91@hotmail.se>
2026-06-29 02:14:14 +0200

    Updated tests to new API

--- tests/test_import_c3d_files.py
+++ tests/test_import_c3d_files.py
@@ -10,9 +10,12 @@ class ImportC3DTestMultipleFiles(unittest.TestCase):
 
     def setUpClass():
         from tests.zipload import Zipload
+        from bpy_extras import anim_utils
+
         Zipload.download_and_extract()
         objs = []
         actions = []
+        channelbags = []
 
         # Parse files
         for filepath in Zipload.get_c3d_filenames('sample00'):
@@ -20,26 +23,30 @@ class ImportC3DTestMultipleFiles(unittest.TestCase):
             bpy.ops.import_anim.c3d(filepath=filepath, print_file=False, perf_mon=False)
             # Fetch loaded objects
             obj = bpy.context.selected_objects[0]
+            action = obj.animation_data.action
+            action_slot = obj.animation_data.action_slot
+            channelbag = anim_utils.action_get_channelbag_for_slot(action, action_slot)
             objs.append(obj)
-            actions.append(obj.animation_data.action)
+            actions.append(action)
+            channelbags.append(channelbag)
 
         ImportC3DTestMultipleFiles.objs = objs
-        ImportC3DTestMultipleFiles.actions = actions
+        ImportC3DTestMultipleFiles.channelbags = channelbags
 
     def test_A_channel_count(self):
         ''' Verify loaded animations has channels
         '''
-        self.assertGreater(len(self.actions), 0)
-        for action in self.actions:
-            self.assertGreater(len(action.fcurves), 0)
+        self.assertGreater(len(self.channelbags), 0)
+        for channelbag in self.channelbags:
+            self.assertGreater(len(channelbag.fcurves), 0)
 
     def test_B_keyframe_count(self):
         ''' Verify each channel has keyframes
         '''
-        self.assertGreater(len(self.actions), 0)
-        for action in self.actions:
-            for i in range(len(action.fcurves)):
-                self.assertGreater(len(action.fcurves[i].keyframe_points), 0)
+        self.assertGreater(len(self.channelbags), 0)
+        for channelbag in self.channelbags:
+            for i in range(len(channelbag.fcurves)):
+                self.assertGreater(len(channelbag.fcurves[i].keyframe_points), 0)
 
 
 if __name__ == '__main__':
